3a9d2b000819f02dadef1e25f17618be764f9862
lihongzhao <hongzhao.li@eeoa.com>
2024-10-09 18:28:47 +0800

    feat: 更新代码

--- pages/example/three/font/index.jsx
+++ pages/example/three/font/index.jsx
@@ -1,51 +1,71 @@
-import { OrbitControls, Text3D } from '@react-three/drei'
+import {
+  Billboard,
+  OrbitControls,
+  PivotControls,
+  Text,
+} from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import React, { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
 import { FontLoader } from 'three/addons/loaders/FontLoader.js'
 
-const Text = () => {
+const TextD = () => {
   const loader = new FontLoader()
 
   useEffect(() => {
     loader.load('/font/bold.json', function (font) {
       const geometry = new TextGeometry('Hello!', {
         font: font,
-        size: 5,
+        size: 3,
         depth: 1,
       })
       text.current.geometry = geometry
     })
+    // pivotControl.current.matrix.makeRotationZ
+    window.pivotControl = pivotControl.current
   }, [])
+  const handleBillboardClick = (e) => {
+    console.log('Billboard clicked', e)
+  }
   useFrame(() => {
-    text.current.rotation.y += 0.01
+    // console.log(pivotControl.current.rotation)
+    // text.current.rotation.z += 0.01
   })
   const text = useRef()
+  const pivotControl = useRef()
+
   return (
     <>
       <mesh ref={text}>
         <meshNormalMaterial />
       </mesh>
-      <Text3D font={'/font/bold.json'} position={new THREE.Vector3(10, 0, 0)}>
-        wooo <meshNormalMaterial />
-      </Text3D>
+      <PivotControls ref={pivotControl} anchor={[0, 0, 0]}>
+        <Text fontSize={1} position={[0, 5, 0]}>
+          旋转文字
+        </Text>
+      </PivotControls>
+
+      <Billboard follow={true} lockX={false} lockY={false} lockZ={false}>
+        <Text fontSize={1} position={[0, 10, 0]} onClick={handleBillboardClick}>
+          面向相机的文字
+        </Text>
+      </Billboard>
     </>
   )
 }
 
 function Com() {
-  return <Text>4</Text>
+  return <TextD></TextD>
 }
 const Sphereg = () => {
   return (
     <div style={{ height: '60vh' }}>
       <Canvas
-        shadows
         dpr={[1, 2]}
         gl={{ alpha: false }}
         scene={{ background: new THREE.Color(0xffffff) }}
-        camera={{ position: [0, 0, -70], fov: 45 }}
+        camera={{ position: [0, 0, -40], fov: 70 }}
       >
         <OrbitControls />
         <axesHelper args={[10]} />
