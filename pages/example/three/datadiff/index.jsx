5829e84e3ce918fab1df41cd0fb17eced9b06e07
lihongzhao <hongzhao.li@eeoa.com>
2024-10-10 15:42:03 +0800

    feat: 更新代码

--- pages/example/three/datadiff/index.jsx
+++ pages/example/three/datadiff/index.jsx
@@ -1,48 +1,94 @@
-import { Box, Environment, OrbitControls, Text } from '@react-three/drei'
+import {
+  Box,
+  Center,
+  Environment,
+  OrbitControls,
+  Plane,
+  Text,
+} from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import * as THREE from 'three'
+import { staticPath } from '../../../utils'
 
-const TextD = () => {
+const Bar = ({ value = 5, text = '未设置', axisX = 0 }) => {
   useEffect(() => {}, [])
-
+  const bar = {
+    width: 3,
+    z: 3,
+    img: {
+      height: 2,
+    },
+  }
   useFrame(() => {
     // console.log(pivotControl.current.rotation)
     // text.current.rotation.z += 0.01
   })
-  const text = useRef()
 
   return (
-    <>
-      <mesh ref={text}>
+    <mesh position={[axisX, 0, 0]}>
+      <Center top>
+        <Box
+          args={[bar.width, value, bar.z]}
+          material={
+            new THREE.MeshBasicMaterial({
+              color: new THREE.Color(0xffffff),
+              map: new THREE.TextureLoader().load(staticPath + '/pano/bar.jpg'),
+            })
+          }
+        />
         <meshNormalMaterial />
-      </mesh>
-      <mesh ref={text}>
-        <Box args={[1, 4, 1]} />
-        <meshNormalMaterial />
-      </mesh>
-      <Text fontSize={1} position={[0, 5, 0]}>
-        旋转文字
+      </Center>
+      <Text
+        anchorY={'top'}
+        fontSize={1}
+        scale={0.5}
+        maxWidth={3}
+        color={'blue'}
+        position={[0, value - bar.img.height, bar.z / 2 + 0.1]}
+      >
+        {text}
       </Text>
-    </>
+      <Plane
+        args={[bar.width, bar.img.height]}
+        position={[0, value - bar.img.height / 2, bar.z / 2 + 0.1]}
+        material={
+          new THREE.MeshBasicMaterial({
+            map: new THREE.TextureLoader().load(staticPath + '/diff/us.jpeg'),
+            side: THREE.DoubleSide,
+          })
+        }
+      ></Plane>
+    </mesh>
   )
 }
 
 function Com() {
-  return <TextD></TextD>
+  const data = [
+    { value: 5, text: '未设置' },
+    { value: 10, text: '已设置' },
+    { value: 15, text: '已设置' },
+  ]
+  return (
+    <>
+      {data.map((item, index) => (
+        <Bar {...item} key={index} axisX={index * 5} />
+      ))}
+    </>
+  )
 }
 const DataDiff = () => {
   return (
     <div style={{ height: '60vh' }}>
       <Canvas
         dpr={[1, 2]}
-        scene={{ background: new THREE.Color(0xffffff) }}
+        scene={{ background: new THREE.Color(0xffffff), castShadow: true }}
         gl={{ alpha: false }}
-        camera={{ position: [0, 0, 40], fov: 70 }}
+        camera={{ position: [0, 0, 20], fov: 70 }}
       >
         <Environment
-          background={true} // can be true, false or "only" (which only sets the background) (default: false)
-          files="/pano/city.jpg" // link to the hdr
+          background={true}
+          files={staticPath + '/pano/jizhuangxhdr.jpg'}
         />
         <OrbitControls />
         <axesHelper args={[10]} />
