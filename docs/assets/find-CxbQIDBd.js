114dbd23a317c9289cf2da1a7bedc011216eac1d
lihongzhao <hongzhao.li@eeoa.com>
2024-10-24 14:12:11 +0800

    feat:更新

---
+++ docs/assets/find-CxbQIDBd.js
@@ -0,0 +1,6 @@
+import{p as i}from"./find_-DWQtdMF4.js";import{u as r,j as n}from"./index-CrfNMau9.js";function o(t){const e={code:"code",h2:"h2",pre:"pre",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h2,{id:"常用-liunx-命令",children:"常用 liunx 命令"}),`
+`,n.jsx(e.h2,{id:"搜索当前目录文件名包含指定字符串的文件",children:"搜索当前目录文件名包含指定字符串的文件"}),`
+`,n.jsx(e.h2,{id:"usage-findsh",children:"Usage: find.sh"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:` **表示包含 name 的
+ find ./ -name "*name*"
+`})})]})}function c(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}const d=Object.freeze(Object.defineProperty({__proto__:null,default:c},Symbol.toStringTag,{value:"Module"})),s={};s.outlineInfo=i;s.main=d;export{s as default};
---
+++ docs/assets/ssrPlugin-bLSWq73h.js
@@ -0,0 +1,1 @@
+import{b as t,R as r,S as c,e as s}from"./index-CrfNMau9.js";var p={id:"vite-pages-theme-doc-antd-ssr",prepare(a){const e=t();return{app:r.createElement(c,{cache:e},a),extractStyle:()=>s(e)}}};export{p as default};
